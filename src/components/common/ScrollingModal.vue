5cae1ed0cf381fa4a755c9c57e62c2d0b5a974a3
apaichon <apaichon@gmail.com>
2021-08-16 01:23:25 +0700

    add modal

---
+++ src/components/common/ScrollingModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <sui-modal v-model="open">
+      <sui-modal-header><slot name="header"> Header </slot></sui-modal-header>
+      <sui-modal-content scrolling image>
+        <sui-modal-description>
+          <slot name="content"> Content </slot>
+        </sui-modal-description>
+      </sui-modal-content>
+      <sui-modal-actions>
+       <slot name="action"> Action </slot>
+      </sui-modal-actions>
+    </sui-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScrollingModal',
+  data () {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    toggle () {
+      this.open = !this.open
+    },
+    openModal () {
+      this.open = true
+    }
+  }
+}
+</script>
--- src/pages/Activities/ActivitiesEditor.vue
+++ src/pages/Activities/ActivitiesEditor.vue
@@ -1,34 +1,48 @@
 <template>
   <div class="ui segment">
-    <h4 class="ui header">Activities Editor</h4>
+    <h2 class="ui header">
+      <i class="settings icon"></i>
+      <div class="content">
+        Activity Editor
+        <button class="ui basic button" @click="onAddClick"> <i class="icon plus"></i> Add</button>
+      </div>
+    </h2>
     <search-data-category :dataItems="categories" @onSearchClick="onSearchClick"></search-data-category>
     <table class="ui celled table">
-  <thead>
-    <tr><th>Id</th>
-    <th>Image</th>
-    <th>Title</th>
-    <th>Category</th>
-    <th>Detail</th>
-    <th>Created At</th>
-    <th>Is Active</th>
-  </tr></thead>
-  <tbody>
-    <tr v-for="a in activitiesTuple" :key="a.id">
-      <td data-label="Id">{{a.id}}</td>
-      <td data-label="Image"><img height="50" width="50" :src="a.thumbnailUrl" /></td>
-      <td data-label="Title">{{a.title}}</td>
-      <td data-label="Category">{{a.category}}</td>
-      <td data-label="Detail">{{a.detail}}</td>
-      <td data-label="CreatedAt">{{a.createdAt}}</td>
-      <td data-label="IsActive">{{a.isActive}}</td>
-    </tr>
-  </tbody>
-</table>
+      <thead>
+        <tr><th>Id</th>
+        <th>Image</th>
+        <th>Title</th>
+        <th>Category</th>
+        <th>Detail</th>
+        <th>Created At</th>
+        <th>Is Active</th>
+        <th>Action</th>
+      </tr></thead>
+      <tbody>
+        <tr v-for="a in activitiesTuple" :key="a.id">
+          <td data-label="Id">{{a.id}}</td>
+          <td data-label="Image"><img height="50" width="50" :src="a.thumbnailUrl" /></td>
+          <td data-label="Title">{{a.title}}</td>
+          <td data-label="Category">{{a.category}}</td>
+          <td data-label="Detail">{{a.detail}}</td>
+          <td data-label="CreatedAt">{{a.createdAt}}</td>
+          <td data-label="IsActive">{{a.isActive}}</td>
+        </tr>
+      </tbody>
+    </table>
+    <scrolling-modal ref="modal"> 
+      <template slot="header">
+        Activity Modal Editor
+      </template>
+      <template slot="content">Content Editorddssrwerwe</template>
+    </scrolling-modal>
   </div>
 </template>
 
 <script>
 import SearchDataCategory from '@/components/search/SearchDataCategory.vue'
+import ScrollingModal from '@/components/common/ScrollingModal.vue'
 import {mapGetters, mapActions} from 'vuex'
 import data from '@/businesses/activities/data'
 
@@ -36,7 +50,7 @@ window.console.log('data', data)
 
 export default {
   name: 'ActivitiesEditor',
-  components: { SearchDataCategory },
+  components: { SearchDataCategory, ScrollingModal },
   computed: {
     ...mapGetters({
       activitiesTuple: 'activities/activitiesTuple'
@@ -45,7 +59,8 @@ export default {
   data () {
     return {
       selectedCategory: null,
-      ...data
+      ...data,
+      modalOpen: false
     }
   },
   async mounted () {
@@ -61,15 +76,21 @@ export default {
       // window.console.log('selectedItem', searchItem)
       let searchConditions = ''
       if (searchItem.textSearch && searchItem.selectedItem.length > 0) {
+        let i = -1
         searchConditions = searchItem.selectedItem.map(a => {
-          return a + `[$like]=%${searchItem.textSearch}%`
+          i++
+          return `$or[${i}][${a}][$like]=%${searchItem.textSearch}%`
         })
         // searchCondition = '?' + searchCondition.substring(0, searchCondition.length - 1)
-        // window.console.log('search condition', searchCondition)
+        // window.console.log('search condition', searchConditions)
       }
       const queryString = '?' + searchConditions.join('&')
       window.console.log(queryString)
       await this.getActivities({textSearch: queryString})
+    },
+    onAddClick () {
+      window.console.log('open Modal')
+      this.$refs.modal.openModal()
     }
   }
 }
